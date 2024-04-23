c5c4ef6d357946bb255ad534a54a6f6e6ec701b3
Vishnu <136763420+vishnu1788@users.noreply.github.com>
2024-04-23 13:06:33 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./index.css";
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-EV7CHM0JJ5"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-EV7CHM0JJ5');
-</script>
   
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
