b01413f3b76dcb2fccae52d13f247183c41ee11e
Vishnu <136763420+vishnu1788@users.noreply.github.com>
2024-06-19 15:55:18 -0400

    Update Navbar.jsx
    ux developer to designer in nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Vishnu &nbsp;
-            <span className='sm:block hidden'> | UI/UX Developer</span>
+            <span className='sm:block hidden'> | UI/UX Designer</span>
           </p>
         </Link>
 
