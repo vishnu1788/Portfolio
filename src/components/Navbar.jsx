428759ad6321b2d495b240ad6c85223f3dfc3619
Vishnu <136763420+vishnu1788@users.noreply.github.com>
2024-08-02 15:32:33 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Vishnu &nbsp;
-            <span className='sm:block hidden'> | Sr Frontend developer</span>
+            <span className='sm:block hidden'> | Sr UI/UX Designer</span>
           </p>
         </Link>
 
